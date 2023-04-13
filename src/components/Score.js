8b345f57a259741d0f6e7b707eb4258087995d8c
Yoo Seungmin <dnjfht@gmail.com>
2023-04-13 23:07:08 +0900

    Score page 수정

--- src/components/Score.js
+++ src/components/Score.js
@@ -5,19 +5,102 @@ import styled from "styled-components";
 const Wrap = styled.div`
   width: 100%;
   height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
-const ImgWrap = styled.img``;
+const ImgWrap = styled.img`
+  width: 320px;
+  height: 320px;
+`;
+
+const Title = styled.h1`
+  margin-top: 40px;
+
+  font-size: 1.6rem;
+  font-weight: 500;
+
+  & > span {
+    padding: 10px 20px;
+    background: rgb(238, 174, 202);
+    background: linear-gradient(
+      90deg,
+      rgba(238, 174, 202, 1) 0%,
+      rgba(148, 187, 233, 1) 100%
+    );
+    border-radius: 10px;
+
+    color: white;
+  }
+`;
+
+const ScoreNumber = styled.h1`
+  margin-top: 40px;
+
+  font-size: 2.4rem;
+  font-weight: 500;
+
+  & > span {
+    padding: 26px 20px;
+    background: rgb(238, 174, 202);
+    background: linear-gradient(
+      90deg,
+      rgba(238, 174, 202, 1) 0%,
+      rgba(148, 187, 233, 1) 100%
+    );
+    border-radius: 100px;
+
+    color: white;
+    font-size: 3rem;
+  }
+`;
+
+const ResultText = styled.p``;
 
 export default function Score() {
   const quiz_list = useSelector((state) => state.quiz.quiz_list);
   const user_answer = useSelector((state) => state.quiz.user_answer);
+  const name = useSelector((state) => state.quiz.name);
+  const result_texts = useSelector((state) => state.quiz.result_text);
+  console.log(result_texts);
 
   const random_number = Math.floor(Math.random() * 10);
+
+  const _score =
+    (100 / quiz_list.length) *
+    user_answer.map((answer, idx) => {
+      if (answer === quiz_list[idx].answer) {
+        return answer;
+      }
+    }).length;
+
+  const score = Math.floor(_score);
+
+  let result_text = "";
+
+  // [40, 60, 80 , 100]
+  Object.keys(result_texts).map((r, idx) => {
+    if (idx === 0) {
+      result_text = result_texts[r];
+    }
+    result_text = parseInt(r) <= score ? result_texts[r] : result_text;
+  });
 
   return (
     <Wrap>
       <ImgWrap src={quiz_list[random_number].image} />
+
+      <Title>
+        <span>{name}</span> 퀴즈에 대한 내 점수는?
+      </Title>
+
+      <ScoreNumber>
+        <span>{score}</span>점
+      </ScoreNumber>
+
+      <ResultText>{result_text}</ResultText>
     </Wrap>
   );
 }
