1b1876eaa9d92c3568d55221e1b355182bd614e9
Yoo Seungmin <dnjfht@gmail.com>
2023-04-14 11:40:00 +0900

    Progress Bar 완성

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
 
 const Wrap = styled.div`
@@ -13,6 +14,29 @@ const Wrap = styled.div`
   );
 `;
 
+const ProgressBar = styled.div`
+  width: ${(props) => props.percent};
+  height: 100%;
+  background: rgb(238, 174, 202);
+  background: linear-gradient(
+    90deg,
+    rgba(238, 174, 202, 1) 0%,
+    rgba(148, 187, 233, 1) 100%
+  );
+
+  transition: all 0.5s;
+`;
+
 export default function Progress() {
-  return <Wrap></Wrap>;
+  const user_answer = useSelector((state) => state.quiz.user_answer);
+  const quiz_list = useSelector((state) => state.quiz.quiz_list);
+
+  const percent = (100 / quiz_list.length) * user_answer.length + "%";
+  console.log(percent);
+
+  return (
+    <Wrap>
+      <ProgressBar percent={percent} />
+    </Wrap>
+  );
 }
